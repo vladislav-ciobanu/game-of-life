88f6bce2cae691d3faff90ac4934d98da5a72915
Vladislav Ciobanu <vagrant@precise64.(none)>
2013-11-24 23:02:47 +0000

    add the Life class, the entry point of the game

---
+++ src/GameOfLife/Life.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace GameOfLife;
+
+/**
+ * Class Life
+ * @package GameOfLife
+ */
+class Life
+{
+    /**
+     * @var GridGenerator
+     */
+    private $gridGenerator;
+
+    /**
+     * @var Replicator
+     */
+    private $replicator;
+
+    /**
+     * @var GridPrinter
+     */
+    private $gridPrinter;
+
+    /**
+     * @param GridGenerator $gridGenerator
+     * @param Replicator $replicator
+     * @param GridPrinter $gridPrinter
+     */
+    public function __construct(GridGenerator $gridGenerator, Replicator $replicator, GridPrinter $gridPrinter)
+    {
+        $this->gridGenerator = $gridGenerator;
+        $this->replicator = $replicator;
+        $this->gridPrinter = $gridPrinter;
+    }
+
+    /**
+     * @param null|int $maxNumberOfGenerations
+     */
+    public function play($maxNumberOfGenerations = null)
+    {
+        $currentGeneration = 1;
+
+        $grid = $this->gridGenerator->generate();
+        $this->gridPrinter->doPrint($grid);
+
+        while (null === $maxNumberOfGenerations || $currentGeneration++ <= $maxNumberOfGenerations) {
+            $newGrid = $this->replicator->replicate($grid);
+            $this->gridPrinter->doPrint($newGrid);
+            $grid = $newGrid;
+        }
+    }
+}
