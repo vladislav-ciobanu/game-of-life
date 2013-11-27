a133a473747e3c8cc84df0a67dff4acd4342a5a6
Vladislav Ciobanu <vagrant@precise64.(none)>
2013-11-27 01:10:39 +0000

    improve cli output of the grid

--- src/GameOfLife/CliGridPrinter.php
+++ src/GameOfLife/CliGridPrinter.php
@@ -4,10 +4,32 @@ namespace GameOfLife;
 
 /**
  * Class CliGridPrinter
+ *
  * @package GameOfLife
  */
 class CliGridPrinter implements GridPrinter
 {
+    /**
+     * @var array
+     */
+    private static $cellStateCharMap = array(
+        CellState::DEAD  => '.',
+        CellState::ALIVE => 'O'
+    );
+
+    /**
+     * @var int
+     */
+    private $displayDelay;
+
+    /**
+     * @param int $displayDelay
+     */
+    public function __construct($displayDelay = 100000)
+    {
+        $this->displayDelay = $displayDelay;
+    }
+
     /**
      * @param Grid $grid
      */
@@ -16,9 +38,9 @@ class CliGridPrinter implements GridPrinter
         $output = PHP_EOL;
 
         /* @var Cell[] $line */
-        foreach ($grid as $line) {
+        foreach ($grid->getCells() as $line) {
             foreach ($line as $cell) {
-                $output .= $cell->getState();
+                $output .= self::$cellStateCharMap[$cell->getState()] . ' ';
             }
             $output .= PHP_EOL;
         }
@@ -26,5 +48,11 @@ class CliGridPrinter implements GridPrinter
         $output .= PHP_EOL;
 
         echo $output;
+
+        ob_flush();
+
+        if ($this->displayDelay > 0) {
+            usleep($this->displayDelay);
+        }
     }
 }
